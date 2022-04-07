860e42793c1c6389032ebf3f332918a1ce37b461
d-e-n-i <denilsoncoutinho9@gmail.com>
2022-04-07 18:06:58 -0300

    To-Do-List

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function AddToList(){
+    let List = document.querySelector("input[name='goals'").value
+    let addList = document.createElement("p")
+    addList.innerText = List
+let check = document.createElement("input")
+ check.type= "checkbox"
+ check.setAttribute("onclick", "check(this)")
+ addList.appendChild(check)
+ 
+
+    let removeButton = document.createElement("button")
+    removeButton.type= "button"
+    removeButton.innerText = "Remover"
+    removeButton.setAttribute("onclick", "RemoveFromList(this)" )
+
+    addList.appendChild(removeButton)    
+ document.querySelector('.todo').appendChild(addList)
+}
+function RemoveFromList(button){ 
+    let premove = button.parentNode
+    document.querySelector('.todo').removeChild(premove)
+}
+
